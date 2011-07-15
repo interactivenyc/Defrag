43cea56e6b8907ee4ef3ca6e99efe7decab532cc
Steve Warren <steve@interactivenyc.com>
2011-07-14 22:39:09 -0400

    new class

---
+++ IOS_Source/Defrag/Utils.h
@@ -0,0 +1,23 @@
+//
+//  Utils.h
+//  Defrag
+//
+//  Created by swarren on 7/9/11.
+//  Copyright 2011 Funny Garbage. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Utils : NSObject {
+@private
+    
+}
+
++ (void)test;
+
++ (NSString *) append:(id) first, ...;
+
++ (void)log;
+
+@end
---
+++ IOS_Source/Defrag/Utils.m
@@ -0,0 +1,64 @@
+//
+//  Utils.m
+//  Defrag
+//
+//  Created by swarren on 7/9/11.
+//  Copyright 2011 Funny Garbage. All rights reserved.
+//
+
+#import "Utils.h"
+
+
+@implementation Utils
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+
++ (void)test
+{
+	NSLog(@"UTILS test");
+	//NSLog(@"%@", [self append:one, @" ", two, nil]);
+}
+
+
+
++ (NSString *) append:(id) first, ...
+{
+    NSString * result = @"";
+    id eachArg;
+    va_list alist;
+    if(first)
+    {
+        result = [result stringByAppendingString:first];
+        va_start(alist, first);
+        while (eachArg = va_arg(alist, id)) 
+			result = [result stringByAppendingString:eachArg];
+        va_end(alist);
+    }
+    return result;
+}
+
+
+
+
+
++ (void)log
+{
+	NSLog(@"UTILS log");
+}
+
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
